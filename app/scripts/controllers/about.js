e788516ae48e803cdb824fda1d1f1802dfd522f7
Janet Maldonado <maldonj2@sutterhealth.org>
2015-10-28 10:36:31 -0700

    Adding project boilerplate code

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name devWorkflowApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the devWorkflowApp
+ */
+angular.module('devWorkflowApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name devWorkflowApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the devWorkflowApp
+ */
+angular.module('devWorkflowApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
